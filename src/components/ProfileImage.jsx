7d1c6266d8ba098fb107e58f0eaf301df85d56aa
maria magdalena <neumariamagdalena@gmail.com>
2024-06-25 21:15:17 +0200

    substitute elements for react components

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,5 @@
+function ProfileImage({ image }) {
+  return <img src={image} alt="profile" className="profile" />;
+}
+
+export default ProfileImage;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,29 +1,26 @@
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+import Message from "./Message";
+import Actions from "./Actions";
+
 function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <img src={tweet.user.image} className="profile" alt="profile" />
+      <ProfileImage image={tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{tweet.user.name}</span>
-            <span className="handle">{tweet.user.handle} </span>
-          </span>
-
-          <span className="timestamp">{tweet.timestamp}</span>
+          <User
+            name={tweet.user.name}
+            handle={tweet.user.handle}
+            className="user"
+          />
+          <Timestamp timestamp={tweet.timestamp} />
         </div>
-
-        <p className="message">{tweet.message}</p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment" data-testid="comment-icon"></i>
-          <i className="fas fa-retweet" data-testid="retweet-icon"></i>
-          <i className="far fa-heart" data-testid="heart-icon"></i>
-          <i className="fas fa-share" data-testid="share-icon"></i>
-        </div>
+        <Message message={tweet.message} />
       </div>
-
+      <Actions />
       <i className="fas fa-ellipsis-h"></i>
     </div>
   );
